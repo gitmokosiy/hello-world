95509c5f062b4e9edc6615a8518ae841b9064b52
Teq <teq@teq.com>
2020-07-16 12:01:47 +0000

    Third

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("HelloOOOO MF!!!, %s!", someone);
+    return String.format("HelloOOOO MF2!!!, %s!", someone);
   }
 }
